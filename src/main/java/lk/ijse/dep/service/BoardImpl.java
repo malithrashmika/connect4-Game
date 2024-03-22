fed2f4f668c387156dd8b21fe532ed5f0f382639
H.Malith Rashmika <156080039+malithrashmika@users.noreply.github.com>
2024-03-22 06:58:08 +0530

    Update BoardImpl.java

--- src/main/java/lk/ijse/dep/service/BoardImpl.java
+++ src/main/java/lk/ijse/dep/service/BoardImpl.java
@@ -2,22 +2,18 @@ package lk.ijse.dep.service;
 
 
 public class BoardImpl implements Board {
-    private int green;
-    private int blue;
 
-
-
-    private Piece[][]pieces;
+    private Piece[][] pieces;
     private BoardUI boardUI;
 
 
     public BoardImpl(BoardUI boardUI) {
-        this.boardUI=boardUI;
-        pieces=new Piece[Num_of_cols][Num_of_rows];
+        this.boardUI = boardUI;
+        pieces = new Piece[Num_of_cols][Num_of_rows];
 
         for (int i = 0; i < Num_of_cols; i++) {
             for (int j = 0; j < Num_of_rows; j++) {
-                pieces[i][j]=Piece.EMPTY;
+                pieces[i][j] = Piece.EMPTY;
             }
         }
 
@@ -26,7 +22,7 @@ public class BoardImpl implements Board {
     public BoardImpl(Piece[][] copiedPieces) {
         Piece[][] pieces1;
         pieces1 = pieces;
-        pieces1 =copiedPieces;
+        pieces1 = copiedPieces;
         this.pieces = pieces1;
     }
 
@@ -36,10 +32,10 @@ public class BoardImpl implements Board {
     }
 
     @Override
-    public int findNextAvalibleSpot(int col) {
+    public int findNextAvailableSpot(int col) {
         for (int i = 0; i < Num_of_rows; i++) {
             if (pieces[col][i] == Piece.EMPTY) {
-                return(i);
+                return (i);
             }
         }
         return -1;
@@ -60,40 +56,47 @@ public class BoardImpl implements Board {
     public boolean existLegalMove() {
         for (int i = 0; i < Num_of_cols; i++) {
             for (int j = 0; j < Num_of_rows; j++) {
-                if(pieces[i][j]==Piece.EMPTY){
-                    return true;
+                if (pieces[i][j] == Piece.EMPTY) {
+                    return false;
                 }
             }
         }
-        return false;
+        return true;
     }
 
     @Override
     public void updateMove(int col, Piece move) throws ArrayIndexOutOfBoundsException {
-        int spot = findNextAvalibleSpot(col);
-        pieces[col][spot]=move;
+        int spot = findNextAvailableSpot(col);
+        pieces[col][spot] = move;
 
     }
+
+    @Override
+    public void updateMove(int col, int row, Piece move) {
+        pieces[col][row] = move;
+    }
+
     @Override
     public Winner findWinner() {
         for (int row = 0; row < Num_of_rows; row++) {
             for (int cols = 0; cols < 3; cols++) {
-                Piece piece=pieces[cols][row];
-                if (piece != Piece.EMPTY && piece==pieces[cols+1][row] && piece==pieces[cols+2][row] && piece==pieces[cols+3][row]){
-                    return new Winner(piece,cols,cols+3,row,row);
+                Piece piece = pieces[cols][row];
+                if (piece != Piece.EMPTY && piece == pieces[cols + 1][row] && piece == pieces[cols + 2][row] && piece == pieces[cols + 3][row]) {
+                    return new Winner(piece, cols, cols + 3, row, row);
                 }
             }
         }
         for (int cols = 0; cols < Num_of_cols; cols++) {
             for (int row = 0; row < 2; row++) {
-                Piece piece=pieces[cols][row];
-                if (piece != Piece.EMPTY && piece == pieces[cols][row+1] && piece == pieces[cols][row+2] && piece == pieces[cols][row+3]){
-                    return new Winner(piece,cols,cols,row,row+3);
+                Piece piece = pieces[cols][row];
+                if (piece != Piece.EMPTY && piece == pieces[cols][row + 1] && piece == pieces[cols][row + 2] && piece == pieces[cols][row + 3]) {
+                    return new Winner(piece, cols, cols, row, row + 3);
                 }
             }
         }
-        return new Winner(Piece.EMPTY,-1,-1,-1,-1);
+        return new Winner(Piece.EMPTY, -1, -1, -1, -1);
     }
+
 
     /*@Override
     public Winner findWinner() {
@@ -134,14 +137,4 @@ public class BoardImpl implements Board {
         }
         return new Winner(Piece.EMPTY);
     }*/
-    @Override
-    public void updateMove(int x, int y, Piece move) {
-        pieces[x][y]=move;
-    }
-
-    @Override
-    public Piece[][] getPieces() {
-        return pieces;
-    }
-
 }
